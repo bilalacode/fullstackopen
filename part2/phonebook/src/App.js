99749d89880e7544c5fdefac1537111a08d76024
bilal <bilalacode@gmail.com>
2022-12-05 18:33:48 +0500

    completed 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Display = ({ persons }) => <ul>{persons.map(person => <li key={person.name.concat(Math.random())}>{person.name} {person.number}</li>)}</ul>
 const Filter = ({ filterName, handleFilterNameChange }) => <div>filter shown with: <input value={filterName} onChange={handleFilterNameChange} /></div>
@@ -19,15 +20,17 @@ const Form = ({ handleForm, newName, handleNameChange, newNum, handleNumChange }
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filterName, updateFilterName] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    }).catch(error => console.log(error))
+  },[])
   const handleForm = (event) => {
     event.preventDefault()
 
