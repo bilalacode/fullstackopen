20daee715f84695af738a202dc50e98ee95b2d67
Bilal <bilala.code@gmail.com>
2023-01-12 18:50:08 +0500

    completed upto 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,82 +1,84 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const morgan = require('morgan')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  response.json(persons)
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has ${persons.length} people.
+  response.send(`Phonebook has ${persons.length} people.
     <br>
     ${new Date()}
     `)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
 
-    person ? response.json(person) : response.status(404).end()
+  person ? response.json(person) : response.status(404).end()
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
 
-    response.send(204).end()
+  response.send(204).end()
 
 })
 
 app.post('/api/persons', (request, response) => {
-    if (!request.body.name) return response.status(400).send(`Name field is invalid`).end()
-    if (!request.body.number) return response.status(400).send(`Phone number invalid`).end()
+  if (!request.body.name) return response.status(400).send(`Name field is invalid`).end()
+  if (!request.body.number) return response.status(400).send(`Phone number invalid`).end()
 
-    for (let person in persons) {
-        if (person.name === request.body.name) return response.status(400).send(`{error: 'name must be unique}`)
-    }
+  for (let person in persons) {
+    if (person.name === request.body.name) return response.status(400).send(`{error: 'name must be unique}`)
+  }
 
-    const generateId = () => 1 + Math.max(...persons.map(person => person.id))
+  const generateId = () => 1 + Math.max(...persons.map(person => person.id))
 
-    const person = request.body
-    person.id = generateId()
+  const person = request.body
+  person.id = generateId()
 
-    console.log(person)
-    persons = persons.concat(person)
+  console.log(person)
+  persons = persons.concat(person)
 
-    response.json(persons)
+  response.json(persons)
 
 })
 
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`App is running on port # ${PORT}`)
+  console.log(`App is running on port # ${PORT}`)
 })
+
